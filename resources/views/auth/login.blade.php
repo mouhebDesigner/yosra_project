44a47eafa497c08e514d9646c8757dcaa2860719
mouheb-ria <mouhebabderrahim@gmail.com>
2022-03-27 14:33:51 +0100

    SOME MODIFICATIONS

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,70 +1,55 @@
 @extends('layouts.app')
 
+@section('breadcamp')
+<div class="breadcrumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="breadcrumb-wrap text-center">
+                    <h1>Login</h1>
+                    <p><a href="index-2.html">Home</a><i class="bx bxs-right-arrow-alt"></i>Login</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="sign-up mt-100 pb-100">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-7 col-lg-10 col-md-12 col-sm-12 col-12">
+                <div class="input-card-wrap input-card-wrap-design-2 text-center ">
+                    <div class="input-card-title">
+                        <h2>Page de connexion</h2>
+                        <p>Nouvel membre? <a href="sign-up.html">S'inscrire ici</a></p>
+                    </div>
+                    <div class="input-card-box mt-100">
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <input type="email" name="email" placeholder="Enter Your Email">
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                            <input id="password-field" name="password" type="password" class="form-control-pass"
+                                name="password" placeholder="Password">
+                            @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                            <i class="bi bi-eye field-icon toggle-password"></i>
+                            <button type="submit" class="mt-50">Connecter</button>
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Avez-vous oublié votre mot de passe?') }}
+                            </a>
+                            @endif
+                        </form>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -64,13 +64,13 @@
                                 <li><a href="contact.html">Contact</a></li>
                             </ul>
                             <div class="menu-btn-wrap d-block d-lg-none">
-                                <a class="common-btn btn-style-1" href="#">Support</a>
+                                <a class="common-btn btn-style-1" href="{{ route('login') }}">Connecter</a>
                             </div>
                         </nav>
                     </div>
                     <div class="col-xl-3 col-lg-3 d-none d-lg-block">
                         <div class="menu-btn-wrap">
-                            <a class="common-btn btn-style-6" href="#">Support</a>
+                            <a class="common-btn btn-style-6" href="{{ route('login') }}">Connecter</a>
                             <div class="menu-toggle-btn">
                                 <ul>
                                     <li></li>
@@ -86,103 +86,8 @@
     </nav>
 
 
-    <div class="menu-toggle-btn-full-shape">
-        <div class="menu-toggle-wrap bg-white">
-            <div class="sidebar-top-area d-flex justify-content-between align-items-center">
-                <div class="cross-icon">
-                    <i class="bx bx-x"></i>
-                </div>
-                <div class="sidebar-logo">
-                    <img src="assets/images/icons/sidebar-logo.svg" alt="">
-                </div>
-            </div>
-            <div class="sidebar-body">
-                <h3 class="sidebar-title"><span>Docbase</span> is a Best Documentation &amp; Knowledge Base Template.
-                </h3>
-                <div class="sidebar-form-area">
-                    <h5>Subscribe Our Newsletter</h5>
-                    <form>
-                        <input type="text" name="name" placeholder="Enter your email">
-                        <button class="sidebar-btn">Subscribe</button>
-                    </form>
-                </div>
-                <div class="sidebar-contact-area">
-                    <h3 class="sidebar-contact-title">Contact</h3>
-                    <ul>
-                        <li>
-                            <h5>Location :</h5>
-                            <p> House#168/170, Road#02, Avenue#01, Mirpur DOHS, Dhaka-1216</p>
-                        </li>
-                        <li>
-                            <h5>Phone : :</h5>
-                            <p>+88 1200 1111 222, +02 0011 2233</p>
-                        </li>
-                        <li>
-                            <h5>Email : :</h5>
-                            <p>infosupport@example.com</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div class="sidebar-social-area">
-                <ul>
-                    <li><a href="#"><i class="bx bxl-facebook"></i></a></li>
-                    <li><a href="#"><i class="bx bxl-twitter"></i></a></li>
-                    <li><a href="#"><i class="bx bxl-instagram"></i></a></li>
-                    <li><a href="#"><i class="bx bxl-pinterest-alt"></i></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
 
-
-    <div class="herobanner-area herobanner-design-2">
-        <div class="social-left-area">
-            <div class="social-text">
-                <p>FollowUs</p>
-            </div>
-            <ul class="social-icon">
-                <li><a href="#"><i class="bx bxl-facebook"></i></a></li>
-                <li><a href="#"><i class="bx bxl-twitter"></i></a></li>
-                <li><a href="#"><i class="bx bxl-instagram"></i></a></li>
-                <li><a href="#"><i class="bx bxl-pinterest-alt"></i></a></li>
-            </ul>
-        </div>
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-xl-10 col-lg-12 col-md-12 col-sm-12 col-12">
-                    <div class="hero-content text-center">
-                        <h1>Search Anything That Your Need</h1>
-                        <p>Search here to get answers to your Questions</p>
-                        <div class="hero-form">
-                            <form action="https://demo.egenslab.com/html/docbase/index.html" class="form-content">
-                                <div class="form-input-area">
-                                    <input type="text" placeholder="Search Here.....">
-                                    <select style="display: none;">
-                                        <option data-display="All">All</option>
-                                        <option value="1">All1</option>
-                                        <option value="2">All2</option>
-                                    </select>
-                                    <div class="nice-select" tabindex="0"><span class="current">All</span>
-                                        <ul class="list">
-                                            <li data-value="All" data-display="All" class="option selected focus">All
-                                            </li>
-                                            <li data-value="1" class="option">All1</li>
-                                            <li data-value="2" class="option">All2</li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </form>
-                            <button>Search</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <img class="shape hero-img-2" src="assets/images/hero-img-2.png" alt="">
-        <img class="shape hero-dot-shape-2" src="assets/images/hero-dot-shape-2.png" alt="">
-        <img class="shape hero-top-left-home-2" src="assets/images/hero-shape-top-left-home-2.png" alt="">
-        <img class="shape hero-dot-shape-2" src="assets/images/hero-dot-shape-2.png" alt="">
-    </div>
+    @yield('breadcamp')
+    
 
 </header>
