d3aa11e4d4d4f653f3ce335324bc1502007fc9df
mouheb-ria <mouhebabderrahim@gmail.com>
2022-05-27 23:28:48 +0100

    DOCUMENT

--- resources/views/admin/includes/aside.blade.php
+++ resources/views/admin/includes/aside.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('/') }}" class="brand-link">
+    <a href="{{ url('/home') }}" class="brand-link">
       <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Gestion de forums</span>
     </a>
@@ -13,7 +13,7 @@
           <img src="{{ asset('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">User name connected </a>
+          <a href="#" class="d-block">{{ Auth::user()->nom }} {{ Auth::user()->prenom }}</a>
         </div>
       </div>
 
